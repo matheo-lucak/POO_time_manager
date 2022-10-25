78a468b26b643bb8fa240fbba4fd8bee0fc88664
matheo lucak <matheo.lucak@epitech.eu>
2022-10-25 19:24:27 +0200

    ✨ Add Clock GET & POST routes
    Clock GET: returns the clock owned by a user
    
    Clock POST: Invert the state of a user clock. Update time when clock status is false. Create new Working Time when clock status is true. If is created is doesn't exists

--- todolist/lib/todolist_web/controllers/clock_controller.ex
+++ todolist/lib/todolist_web/controllers/clock_controller.ex
@@ -4,40 +4,16 @@ defmodule TodolistWeb.ClockController do
   alias Todolist.TimeManagement
   alias Todolist.TimeManagement.Clock
 
-  action_fallback TodolistWeb.FallbackController
+  action_fallback(TodolistWeb.FallbackController)
 
-  def index(conn, _params) do
-    clocks = TimeManagement.list_clocks()
-    render(conn, "index.json", clocks: clocks)
-  end
-
-  def create(conn, %{"clock" => clock_params}) do
-    with {:ok, %Clock{} = clock} <- TimeManagement.create_clock(clock_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.clock_path(conn, :show, clock))
-      |> render("show.json", clock: clock)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    clock = TimeManagement.get_clock!(id)
+  def index(conn, %{"userID" => userId}) do
+    clock = TimeManagement.get_user_clock(userId)
     render(conn, "show.json", clock: clock)
   end
 
-  def update(conn, %{"id" => id, "clock" => clock_params}) do
-    clock = TimeManagement.get_clock!(id)
-
-    with {:ok, %Clock{} = clock} <- TimeManagement.update_clock(clock, clock_params) do
+  def toggle(conn, %{"userID" => userId}) do
+    with {:ok, %Clock{} = clock} <- TimeManagement.toggle_user_clock(userId) do
       render(conn, "show.json", clock: clock)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    clock = TimeManagement.get_clock!(id)
-
-    with {:ok, %Clock{}} <- TimeManagement.delete_clock(clock) do
-      send_resp(conn, :no_content, "")
     end
   end
 end
