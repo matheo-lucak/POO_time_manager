0834e4d9259ea04afe400ec1d648672c2ed3647e
crow <gabriel1.jobert@epitech.eu>
2022-10-26 14:27:57 +0200

    Init vue js project

---
+++ time-manager/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import AppHeader from '@/components/shared/AppHeader.vue';
+</script>
+
+<template>
+  
+  <AppHeader />
+  <RouterView />
+
+</template>
+
+<style>
+
+
+
+</style>
