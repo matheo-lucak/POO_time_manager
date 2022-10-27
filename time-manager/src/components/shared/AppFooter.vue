933361499e2e9ee098bfa12efbc5eed14d8b77b8
crow <gabriel1.jobert@epitech.eu>
2022-10-27 11:40:04 +0200

    FIX: skeleton / template done

---
+++ time-manager/src/components/shared/AppFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        
+        <div class="footer-content">
+            <div class="copyright">
+
+            </div>
+        </div>
+        
+    </footer>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.footer-content {
+  max-width: 1140px;
+  padding: 8px;
+  margin: 0 auto;
+}
+
+.copyrights {
+  font-size: 9px;
+}
+.footer-options {
+  font-size: 12px;
+  cursor: pointer;
+}
+
+</style>
--- time-manager/src/router/index.ts
+++ time-manager/src/router/index.ts
@@ -1,21 +1,19 @@
+import WorkingTimesVue from '@/components/features/WorkingTimes/WorkingTimes.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/',
+      name: 'workingtimes',
+      component: WorkingTimesVue
+    },
+    {
+      path: '/chart-manager',
+      name: 'chart-manager',
+      component: () => import('@/components/features/ChartManager/ChartManager.vue')
+    },
   ]
 })
 
