428ecfd4e08562287a37ea1a1f7ea7b2ee913756
mangel9742 <geoffroy.yann@epitech.eu>
2022-10-28 11:27:59 +0200

    [add] finally instantiated a store [TODO] test with the api

--- time-manager/src/components/features/Clock/Clock.vue
+++ time-manager/src/components/features/Clock/Clock.vue
@@ -4,7 +4,7 @@
 
   <div id="clock">
 
-    Today's getNow is
+    <div v-if="clockStore.clock.status === true">ClockActivated</div>
     <button v-on:click="clockInFunction">ClockIn</button>
     <img v-if="clockIn === true" :src=image alt="Clock is ticking"/>
   </div>
@@ -12,42 +12,30 @@
 
 <script lang="ts">
 
-// import clockInImage from "./assets/running-clock-02.gif";
-// import {useClockStore} from "@/core/stores/clock.store";
-// import type { Clock }  from "@/core/interfaces/clock.interface";
-// import ClocksServices from "@/core/api/clocks.service"
+import { useClockStore } from "@/core/stores/clock.store";
+import { defineComponent } from "vue"
+import ClocksServices from "@/core/api/clocks.service";
+import type {Clock} from "@/core/interfaces/clock.interface";
 
-export default {
+export default defineComponent( {
   name: 'ClockManager',
   props: [
       "userID"
   ],
   data() {
     return {
-      clock: {},
       userID: 0,
       clockIn: true
     }
   },
-  mounted() {
-    this.userID
-  //     const today = new Date();
-  //     const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  //     const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  //     const { initClock } = useClockStore();
-  //
-  //     const clockService = new ClocksServices();
-  //
-  //   clockService.getClock(this.userID).then((response : Clock) => {
-  //       initClock(response);
-  //     }).catch( error => {
-  //       //TODO: handle error
-  //       console.log("error");
-  //     });
-  // },
-  // methods: {
-  //
+  setup()  {
+    const clockStore = useClockStore();
+    return { clockStore }
+  },
+
+  methods: {
+
   }
-}
+})
 
 </script>
