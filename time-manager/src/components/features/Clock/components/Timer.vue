7db3ab48504bdc6fad5e1f082c7194de7acd96ab
mangel9742 <geoffroy.yann@epitech.eu>
2022-11-02 09:45:35 +0100

    [add] baby Composant for Clock vue

---
+++ time-manager/src/components/features/Clock/components/Timer.vue
@@ -0,0 +1,30 @@
+<template>
+  <label for="toggle_button">
+    <span>On</span>
+    <span>Off</span>
+    <h2>Maggle vla ta clock : {{ formatDate(date) }}</h2>
+    <input type="checkbox" id="toggle_button">
+  </label>
+</template>
+<script>
+import {defineComponent} from "vue";
+import moment from 'moment';
+
+export default defineComponent({
+  name: "Timer",
+  props: {
+    date: String,
+  },
+  methods: {
+    formatDate: function (date) {
+      console.log(date)
+      if (date) {
+        return moment(String(date)).format('MM/DD/YYYY, h:mm:ss a');
+      } else {
+        return "DJANGOaaaaaaa";
+      }
+    }
+  }
+
+})
+</script>
