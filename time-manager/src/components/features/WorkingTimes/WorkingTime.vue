81ff42f9e82b4a9449e32141c67fe3bde2777a18
crow <gabriel1.jobert@epitech.eu>
2022-11-02 14:12:08 +0100

    wip xorkingtimes

--- time-manager/src/components/features/WorkingTimes/WorkingTime.vue
+++ time-manager/src/components/features/WorkingTimes/WorkingTime.vue
@@ -1,9 +1,34 @@
 <template>
 
+    <div class="feature-container">
+
+        <div class="time-pick" id="start">
+            <div>
+                <h2>Start</h2>
+                <Datepicker v-model="startDate" minimumView="time" maximumView="day"/>
+                <span>{{getStartTime}}</span>
+            </div>
+        </div>
+
+        <div class="time-pick" id="end">
+            <div>
+                <h2>End</h2>
+                <Datepicker v-model="endDate" minimumView="time" maximumView="day"/>
+                <span>{{getEndTime}}</span>
+            </div>
+        </div>
+
+        <div class="total">
+            Heures travaillées : {{hoursBetween}}
+        </div>
+
+    </div>
+    
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import Datepicker from 'vue3-datepicker';
+import { ref, defineComponent } from 'vue';
 
 export default defineComponent({
     data() {
@@ -11,14 +36,52 @@ export default defineComponent({
             id: 0,
         }
     },
-    props: {
+    setup() {
+        const startDate = ref(new Date());
+        const endDate = ref(new Date());
         
+        return {
+            startDate,
+            endDate
+        }
     },
-    beforeMount() {
-        this.id;
+    computed: {
+        getStartTime() {
+            return this.startDate.toLocaleTimeString();
+        },
+        getEndTime() {
+            return this.endDate.toLocaleTimeString();
+        },
+        hoursBetween() {
+            let millisecondsPerHour = 60 * 60 * 1000;
+            let diff = this.treatAsUTC(this.endDate.toString()).getTime() - this.treatAsUTC(this.startDate.toString()).getTime();
+            if(diff > millisecondsPerHour)
+                return diff / millisecondsPerHour + ' h';
+            return diff / (millisecondsPerHour / 60) + ' min';
+        }
     },
+    components: {
+        Datepicker
+    },
+    methods: {
+        treatAsUTC(date: string) {
+            let result = new Date(date);
+            result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+            return result;
+        },
+        
+    }
 })
 </script>
 
 <style>
+.time-pick {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding:21px;
+}
+
+
 </style>
