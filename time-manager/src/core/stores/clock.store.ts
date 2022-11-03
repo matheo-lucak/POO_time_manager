a1e4596c16c52f7bd10095495b5ffec134e4bfac
mangel9742 <geoffroy.yann@epitech.eu>
2022-11-03 14:43:16 +0100

    [fix] Clock vue final version

---
+++ time-manager/src/core/stores/clock.store.ts
@@ -0,0 +1,56 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import type {Clock} from "@/core/interfaces/clock.interface";
+import ClocksServices from "@/core/api/clocks.service"
+import type { User } from "@/core/interfaces/user.interface"
+
+type ClockStore =
+    {
+        clock: Clock;
+        clockServices: ClocksServices;
+    }
+
+export const useClockStore = defineStore('clock', {
+    state: (): ClockStore => <ClockStore>({
+        clock: {
+            time: "",
+            status: false,
+        },
+        clockServices: new ClocksServices()
+
+    }),
+    getters: {
+        getClock(state) {
+            return this.clock;
+        }
+
+    },
+    actions: {
+
+        fetchClock(userID: number) {
+            this.clockServices.getClock(userID).then((response: Clock) => {
+                this.clock = response;
+            }).catch((e) => {
+                console.log(e)
+            })
+        },
+
+        submitClock(userID: number) {
+            this.clockServices.postClock(userID).then((response: Clock) => {
+                this.clock = response;
+            }).catch((e) => {
+                console.log(e)
+            });
+
+        },
+
+        refreshClock(userID: number) {
+            this.clockServices.postClock(userID).then((response: Clock) => {
+                this.clock = response;
+            }).catch((e) => {
+                console.log(e)
+            });
+
+        }
+    }
+})
