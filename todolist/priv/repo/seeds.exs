f3e560b5d123cd78a82fff2bcbc934291348fdb0
matheo-lucak <60272246+matheo-lucak@users.noreply.github.com>
2022-11-06 16:32:12 +0100

    🌱 Seeds multiple users at DB creation (#13)

--- todolist/priv/repo/seeds.exs
+++ todolist/priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Todolist.Account
+
+Account.create_user(%{
+  email: "general_manager_1@epitech.eu",
+  username: "General Manager 1",
+  password: "general manager password",
+  password_confirmation: "general manager password",
+  role: "general_manager"
+})
+
+Account.create_user(%{
+  email: "manager_1@epitech.eu",
+  username: "Manager 1",
+  password: "manager password",
+  password_confirmation: "manager password",
+  role: "manager"
+})
+
+Account.create_user(%{
+  email: "user_1@epitech.eu",
+  username: "User 1",
+  password: "user password",
+  password_confirmation: "user password",
+  role: "user"
+})
