42ba68564584cf7293ec3029d8afee9329b5dcd1
mangel9742 <geoffroy.yann@epitech.eu>
2022-11-07 11:31:53 +0100

    [add] LoginVue, ManagementVue and RegisterVue

---
+++ time-manager/src/components/features/User/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <form @submit.prevent="Login">
+      <input type="text" placeholder="Email" v-model="email" />
+      <input type="text" placeholder="Password" v-model="password" />
+      <input type="submit" value="Login" />
+      <p>Need an account? <router-link to="/auth/register">Register here!
+      </router-link></p>
+    </form>
+</div>
+</template>
+<script lang="ts">
+
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: "Login",
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const Login = () => {
+
+    }
+    return {
+      Login,
+      email,
+      password
+    }
+  },
+})
+</script>
+<style>
+
+</style>
---
+++ time-manager/src/components/features/User/components/Register.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <form @submit.prevent="Register">
+      <input type="text" placeholder="Email" v-model="email" />
+      <input type="text" placeholder="Username" v-model="username" />
+      <input type="text" placeholder="Password" v-model="password" />
+      <input type="submit" value="Register" />
+      <p>Have an account? <router-link to="/auth/login">Login here!
+      </router-link></p>
+    </form>
+  </div>
+</template>
+<script lang="ts">
+
+import {defineComponent, ref} from "vue";
+import {useUserStore} from "@/core/stores/user.store";
+
+export default defineComponent({
+  name: "Register",
+  setup() {
+    const email = ref("");
+    const username = ref("");
+    const password = ref("");
+    const userStore = useUserStore();
+
+    const Register = () => {
+      userStore.registerUser(email.value, username.value, password.value);
+    }
+    return {
+      Register,
+      email,
+      username,
+      password
+    }
+  },
+})
+</script>
+<style>
+
+</style>
