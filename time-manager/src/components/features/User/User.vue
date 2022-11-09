00c9a937f34c0e8436f752d63b81c6e4148dc476
mangel9742 <geoffroy.yann@epitech.eu>
2022-11-09 16:35:59 +0100

    [fix] clock, user, login and register, preparing to merge on main

--- time-manager/src/components/features/User/User.vue
+++ time-manager/src/components/features/User/User.vue
@@ -1,30 +1,63 @@
 <template>
-
-
+  <div>
+    <nav>
+      <Dropdown />
+    </nav>
+  </div>
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import {useUserStore} from "@/core/stores/user.store";
+import Dropdown from "@/components/shared/Dropdown.vue";
 
 export default defineComponent({
-    // TODO: wrap login register + crud user in here, openable from header
+  name: 'User',
+  components: {Dropdown},
+  // TODO: wrap login register + crud user in here, openable from header
 
-    // TODO: components we'll wrap in the sidebar, if current user is general manager promote / demote routes implemented, if not just regular statistics + crud for the selected user
-    data() {
-        return {
-
-        }
-    },
-    beforeMount() {
-
-    },
-    computed: {
-
-    },
-    methods: {
+  // TODO: components we'll wrap in the sidebar, if current user is general manager promote / demote routes implemented, if not just regular statistics + crud for the selected user
+  setup() {
+    const userStore = useUserStore();
+    return { userStore }
+  },
+  data() {
+    return {
 
     }
+  },
+  beforeMount() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
 })
 </script>
+
+<style scoped>
+nav .menu-item {
+  color: #ffffff;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.4s;
+}
+
+div. menu-item.active,
+div. menu-item:hover {
+  background-color: #3c4462;
+  border-bottom-color: #a94442;
+}
+
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
